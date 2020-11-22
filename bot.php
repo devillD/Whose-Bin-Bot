56a6a01b5ba63ca92e96f52645ca0194bca16aac
Benchamxd <73537720+Benchamxd@users.noreply.github.com>
2020-11-22 21:33:37 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,6 +10,7 @@ set_time_limit(0);
 
 flush();
 ##------------------------------##
+$API_KEY = $_ENV["BOT_TOKEN"]; // bot token
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
