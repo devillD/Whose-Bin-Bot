87d647f6f9e325d8bddaaabc83cf69884bf7b9ef
Benchamxd <73537720+Benchamxd@users.noreply.github.com>
2020-11-22 12:11:27 +0530

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,113 @@
+<?php
+/*
+BY:- @BenchamXD
+
+CHANNEL:- @IndusBots
+*/
+error_reporting(0);
+
+set_time_limit(0);
+
+flush();
+$API_KEY = 'YOUR BOT TOKEN'; //Your token
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+ function sendmessage($chat_id, $text, $model){
+	bot('sendMessage',[
+	'chat_id'=>$chat_id,
+	'text'=>$text,
+	'parse_mode'=>$mode
+	]);
+	}
+	
+//==============BENCHAM======================//
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$message_id = $update->message->id;
+$chat_id = $message->chat->id;
+$name = $from_id = $message->from->first_name;
+$from_id = $message->from->id;
+$text = $message->text;
+$fromid = $update->callback_query->from->id;
+$username = $update->message->from->username;
+$chatid = $update->callback_query->message->chat->id;
+$callback_query = $update->callback_query->data;
+$messageid = $update->callback_query->message->message_id;
+$reply = $update->message->reply_to_message->message_id;
+  $date = date("d/m/y");
+$AdminID = 1222113933; //admin user id get it from @MissRose_bot 
+ $logchnl = -1001282868218;
+if($text == '/start')
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"Welcome to bin checker
+
+Use /bin xxxxx to check the bin.
+
+You can also make a bot like this from here:- https://github.com/BenchamXd/Bin-Checker"
+]);
+if(strpos($text,"/bin") !== false){ 
+$bin = trim(str_replace("/bin","",$text)); 
+
+$data = json_decode(file_get_contents("https://lookup.binlist.net/$bin"),true);
+$bank = $data['bank']['name'];
+$country = $data['country']['alpha2'];
+$currency = $data['country']['currency'];
+$emoji = $data['country']['emoji'];
+$scheme = $data['scheme'];
+$Brand = $data['brand'];
+$type = $data['type'];
+ if($data['scheme']){
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"***VALID BIN✅
+                
+Bin: $bin
+
+Type: $scheme
+
+Brand : $Brand
+
+Bank: $bank
+
+Country: $country $emoji
+
+Currency: $currency
+
+Credit/Debit:$type
+
+Checked By @$username***",
+'parse_mode'=>"MarkDown",
+ 'reply_markup' =>  json_encode([
+                'inline_keyboard' => [
+                 [['text' => "BACK",'callback_data'=>"helpme"]],
+                 ]])
+
+]);
+    }
+else {
+bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"INVALID BIN❌",
+               
+]);
+}
+}
+/*
+BY:- @BenchamXD
+
+CHANNEL:- @IndusBots
+*/
