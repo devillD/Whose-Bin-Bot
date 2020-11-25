6af335266e63486778d21c721aacd6e7192d2bce
Benchamxd <73537720+Benchamxd@users.noreply.github.com>
2020-11-25 17:19:36 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ error_reporting(0);
 set_time_limit(0);
 
 flush();
-$API_KEY = $_ENV['API_KEY']; //Your token
+$API_KEY = $_ENV['BOT_TOKEN']; //Your token
 ##------------------------------##
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
